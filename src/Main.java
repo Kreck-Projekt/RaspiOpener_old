a2bf06bb55e2c54317fbda6102227d5e2a1f8670
niko <nikobo2003@gmail.com>
2021-01-27 12:00:51 +0100

    deleted useless while loop

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,28 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main {
+    private static StringWriter sw = new StringWriter();
+    private static PrintWriter pw = new PrintWriter(sw);
     private static DateFormat dateF = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-    private static Date date = new Date();
     public static void main(String[] args) throws Exception {
-        while(true) {
 
-            try{
-                System.out.println("Starting...");
-                // TCP Server starten...
-                TCPServer.main(null);
-            }
-            catch(Exception e){
-                e.printStackTrace();
-                System.out.println("Closing...?");
-                Printer.printToFile(dateF.format(date) + ": server crashed?" + e.getStackTrace(), new PrintWriter(new BufferedWriter(new FileWriter("log.txt", true))));;
-            }
+        try{
+            System.out.println("Starting...");
+            // TCP Server starten...
+            TCPServer.run();
         }
+        catch(Exception e){
+            e.printStackTrace(pw);
+            System.out.println("Closing...?");
+            Printer.printToFile(dateF.format(new Date()) + ": server crashed?" + sw.toString(), new PrintWriter(new BufferedWriter(new FileWriter("log.txt", true))));
+
+        }
+
     }
 }
