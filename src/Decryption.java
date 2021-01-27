73fd0655b9d551a1cd74172674f37f3e92de611b
niko <nikobo2003@gmail.com>
2021-01-27 22:34:59 +0100

    removed unnecessary code

--- src/Decryption.java
+++ src/Decryption.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.xml.bind.DatatypeConverter;
 
-public class Decryption{
+public class Decryption {
     private static final String ENCRYPT_ALGO = "AES/GCM/NoPadding";
     private static final int TAG_LENGTH_BIT = 128;
 
@@ -24,14 +24,14 @@ public class Decryption{
 
     public static String decrypt(String key, String nonce, String msg) throws Exception {
         String hexKey = key;
-        byte [] encodedKey = DatatypeConverter.parseHexBinary(hexKey);
+        byte[] encodedKey = DatatypeConverter.parseHexBinary(hexKey);
         SecretKey secretKey = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
 
         String hexNonce = nonce;
-        byte [] byteNonce = DatatypeConverter.parseHexBinary(hexNonce);
+        byte[] byteNonce = DatatypeConverter.parseHexBinary(hexNonce);
 
         String hexMsgEncrypted = msg;
-        byte [] encryptedText = DatatypeConverter.parseHexBinary(hexMsgEncrypted);
+        byte[] encryptedText = DatatypeConverter.parseHexBinary(hexMsgEncrypted);
 
         Cipher cipher = Cipher.getInstance(ENCRYPT_ALGO);
         cipher.init(Cipher.DECRYPT_MODE, secretKey, new GCMParameterSpec(TAG_LENGTH_BIT, byteNonce));
@@ -41,23 +41,5 @@ public class Decryption{
 
         return decryptedText;
     }
-
-
-
-
-    public static String decrypt(byte[] cText, SecretKey secret, byte[] iv) throws Exception {
-
-        Cipher cipher = Cipher.getInstance(ENCRYPT_ALGO);
-        cipher.init(Cipher.DECRYPT_MODE, secret, new GCMParameterSpec(TAG_LENGTH_BIT, iv));
-        byte[] plainText = cipher.doFinal(cText);
-        return new String(plainText, UTF_8);
-
-    }
 }
 
-
-class EncryptorAesGcm {
-
-
-
-}
