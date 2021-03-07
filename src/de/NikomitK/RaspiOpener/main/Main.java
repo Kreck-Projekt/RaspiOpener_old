c286446473579b333ba9bcde26ff40d39b3b79cd
Niko <nikobo2003@gmail.com>
2021-03-07 21:36:56 +0100

    added starting argument -r to reset the pi when launching the application

--- src/de/NikomitK/RaspiOpener/main/Main.java
+++ src/de/NikomitK/RaspiOpener/main/Main.java
@@ -12,7 +12,9 @@ public class Main {
     private static PrintWriter pw = new PrintWriter(sw);
     private static DateFormat dateF = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     public static void main(String[] args) throws Exception {
-
+        if(args.length>0 /*&& args[0].equals("-r")*/){
+            BashIn.exec("sudo rm keyPasStore.txt");
+        }
         try{
             Scanner sc = new Scanner(new File("keyPasStore.txt"));
         }
--- src/de/NikomitK/RaspiOpener/main/TCPServer.java
+++ src/de/NikomitK/RaspiOpener/main/TCPServer.java
@@ -88,11 +88,21 @@ class TCPServer {
                 fromclient = fromClient.readLine();
                 System.out.println("Recieved: " + fromclient);
                 try {
-                    if (fromclient.charAt(1) != ':') {
+                    if (fromclient.charAt(1) != ':' && fromclient.equals("null")) {
+                        try {
+                            toClient.println("Ey (K-K-Kingsake) Ey, Pasha, ey, ja, ey (YungGlizzy) Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Prada Çanta voller Haze Komplett Kafa, outta Race Braune Locken, grüne Augen Shababs sind am Botten, no face, no case, ey Halbe Kiste im Toyota gebunkert Einundsechzig, keiner sober, nur Kundschaft Neue Ringe, neue Eyes Einundsechzig, gerade heiß Shawty, sag' mir, wie du heißt Hab' vergessen, tut mir leid An Sarrazin will ich verdienen, bin in Berlin Sein Sohn holt jede Woche Tilidin Mehringdamm, Saka-Wasser oder Lean Müsteris schreiben mir: „Komm' vorbei“ Und alle paar Monate flieg' ich Türkei Ich ess' einen Adana oder auch zwei Abi hat Katana an seinem Bein und Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Ey (K-K-Kingsake) Ey, Pasha, ey, ja, ey (YungGlizzy) Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Prada Çanta voller Haze Kafa, outta Race Braune Locken, grüne Augen Shababs sind am Botten, no face, no case, ey Halbe Kiste im Toyota gebunkert Einundsechzig, keiner sober, nur Kundschaft Neue Ringe, neue Eyes Einundsechzig, gerade heiß Shawty, sag' mir, wie du heißt Hab' vergessen, tut mir leid An Sarrazin will ich verdienen, bin in Berlin Sein Sohn holt jede Woche Tilidin Mehringdamm, Saka-Wasser oder Lean Müsteris schreiben mir: „Komm' vorbei“ Und alle paar Monate flieg' ich Türkei Ich ess' einen Adana oder auch zwei Abi hat Katana an seinem Bein und Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen\n");
+                        } catch (Exception e) {
+                            System.out.println("HIER BIN ICH GECRASHT");
+                        }
                         connected.close();
                         continue;
                     } //checks if the sent message is a command#
                 } catch (Exception e) {
+                    try {
+                        toClient.println("Ey (K-K-Kingsake) Ey, Pasha, ey, ja, ey (YungGlizzy) Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Prada Çanta voller Haze Komplett Kafa, outta Race Braune Locken, grüne Augen Shababs sind am Botten, no face, no case, ey Halbe Kiste im Toyota gebunkert Einundsechzig, keiner sober, nur Kundschaft Neue Ringe, neue Eyes Einundsechzig, gerade heiß Shawty, sag' mir, wie du heißt Hab' vergessen, tut mir leid An Sarrazin will ich verdienen, bin in Berlin Sein Sohn holt jede Woche Tilidin Mehringdamm, Saka-Wasser oder Lean Müsteris schreiben mir: „Komm' vorbei“ Und alle paar Monate flieg' ich Türkei Ich ess' einen Adana oder auch zwei Abi hat Katana an seinem Bein und Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Ey (K-K-Kingsake) Ey, Pasha, ey, ja, ey (YungGlizzy) Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Prada Çanta voller Haze Kafa, outta Race Braune Locken, grüne Augen Shababs sind am Botten, no face, no case, ey Halbe Kiste im Toyota gebunkert Einundsechzig, keiner sober, nur Kundschaft Neue Ringe, neue Eyes Einundsechzig, gerade heiß Shawty, sag' mir, wie du heißt Hab' vergessen, tut mir leid An Sarrazin will ich verdienen, bin in Berlin Sein Sohn holt jede Woche Tilidin Mehringdamm, Saka-Wasser oder Lean Müsteris schreiben mir: „Komm' vorbei“ Und alle paar Monate flieg' ich Türkei Ich ess' einen Adana oder auch zwei Abi hat Katana an seinem Bein und Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen\n");
+                    } catch (Exception ex) {
+                        System.out.println("HIER BIN ICH GECRASHT");
+                    }
                     connected.close();
                     continue;
                 }
@@ -106,96 +116,82 @@ class TCPServer {
                     connected.close();
                     break;
                 }
-//                boolean first /*the first found semicolon*/ = false;
-//                for (int i = 0; i < param.length(); i++) {
-//                    if (!first && param.charAt(i) == ';') first = true;
-//                    else if (first && param.charAt(i) == ';') nonce = param.substring(i + 1);
-//                }
-//                int posPas = -1;
 
-//                can this be removed???
-                switch (fromclient.charAt(0)) {
-                    case 'n': //irrelevant
-                        break;
-
-                    case 'k': //storeKey done
-                        // Command syntax: "k:<key>"
-                        boolean keyStored = false;
-                        if (((key == null || key.equals("")) && param.length() == 32) && secured)
-                            keyStored = handler.storeKey(param);
-                        if(!keyStored) System.out.println("Das hat nicht geklappt! :(");
-                            key = handler.key;
-                        break;
-
-                    case 'p': //storePW done
-                        // Command syntax: "p:(<hash>);<nonce>"
-                        boolean pwStored = handler.storePW(param);
-                        if(!pwStored) System.out.println("Das hat nicht geklappt! :(");
-                        oriHash = handler.oriHash;
-                        break;
-
-                    case 'c': //changePW done
-                        // Command syntax: "c:(<oldHash>;<newHash>);<nonce>"
-                        boolean changed = handler.changePW(param);
-                        if(!changed) System.out.println("Das hat nicht geklappt! :(");
-                        break;
-
-                    case 's': // setOTP done
-                        // Command syntax "s:(<otp>;<hash>);<nonce>>"
-                        boolean otpSet = handler.setOTP(param);
-                        if(!otpSet) System.out.println("Das hat nicht geklappt! :(");
-                        otps = handler.otps;
-                        break;
-
-                    case 'e': // einmalöffnung done
-                        // Command syntax: "e:<otp>;<time>"
-                        boolean otOpened = handler.einmalOeffnung(param);
-                        if(!otOpened) System.out.println("Das hat nicht geklappt! :(");
-                        otps = handler.otps;
-                        break;
-
-                    case 'a': // a für "password action" aka halts maul justin und formulier gescheit was du sagen willst du keks
-                        System.out.println("PaSsWoRd AcTiOn"); // this case is irrelevant
-                        System.out.println("Junge sag doch einfach, dass das als öffnen gemeint war"); // just ignore this
-                        Printer.printToFile(dateF.format(new Date()) + ": Das hätte definitiv nicht passieren sollen? #weirdflexbutok", "log.txt", true);
-                        break;
-
-                    case 'o': // Open  done
-                        // Command syntax: "o:(<hash>;<time>);<nonce>"
-                        boolean opened = handler.open(param);
-                        if(!opened) System.out.println("Das hat nicht geklappt! :(");
-                        break;
-
-                    case 'r': //reset
-                        // Command syntax: "r:(<hash>);<nonce>"
-                        boolean isReset = handler.reset(param);
-                        if(!isReset) System.out.println("Das hat wohl nicht geklappt! :(");
-                        key = handler.key;
-                        oriHash = handler.oriHash;
-                        break;
-
-                    case 'H': // "how are you", get's called from alivekeeper, never from user lmao
-                        toClient.println("I'm fine, thanks");
-                        break;
-
-                    default:
-                        //also irrelevant
-                        System.out.println("hat wohl nich gegeht ¯\\_(ツ)_/¯ ");
-                        Printer.printToFile(dateF.format(new Date()) + ": Wie zur Hölle bist du hier gelandet??? #nochweirdererflex", "log.txt", true);
-                        Printer.printToFile(dateF.format(new Date()) + ": The whole message was: " + fromclient, "log.txt", true);
-                        toClient.println("What do you want from this poor Server? \uD83E\uDD7A");
-                        break;
-                }
-
-
-                //Justin wollte iwie ne nachricht idk
-
+                boolean worked = false;
                 try {
-                    toClient.println("Ey (K-K-Kingsake) Ey, Pasha, ey, ja, ey (YungGlizzy) Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Prada Çanta voller Haze Komplett Kafa, outta Race Braune Locken, grüne Augen Shababs sind am Botten, no face, no case, ey Halbe Kiste im Toyota gebunkert Einundsechzig, keiner sober, nur Kundschaft Neue Ringe, neue Eyes Einundsechzig, gerade heiß Shawty, sag' mir, wie du heißt Hab' vergessen, tut mir leid An Sarrazin will ich verdienen, bin in Berlin Sein Sohn holt jede Woche Tilidin Mehringdamm, Saka-Wasser oder Lean Müsteris schreiben mir: „Komm' vorbei“ Und alle paar Monate flieg' ich Türkei Ich ess' einen Adana oder auch zwei Abi hat Katana an seinem Bein und Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Ey (K-K-Kingsake) Ey, Pasha, ey, ja, ey (YungGlizzy) Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Prada Çanta voller Haze Kafa, outta Race Braune Locken, grüne Augen Shababs sind am Botten, no face, no case, ey Halbe Kiste im Toyota gebunkert Einundsechzig, keiner sober, nur Kundschaft Neue Ringe, neue Eyes Einundsechzig, gerade heiß Shawty, sag' mir, wie du heißt Hab' vergessen, tut mir leid An Sarrazin will ich verdienen, bin in Berlin Sein Sohn holt jede Woche Tilidin Mehringdamm, Saka-Wasser oder Lean Müsteris schreiben mir: „Komm' vorbei“ Und alle paar Monate flieg' ich Türkei Ich ess' einen Adana oder auch zwei Abi hat Katana an seinem Bein und Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen Shababs botten, grüne Augen, braune Locken Tn's rocken, halbe Kiste, wenn wir shoppen\n");
-                } catch (Exception e) {
-                    System.out.println("HIER BIN ICH GECRASHT");
+                    switch (fromclient.charAt(0)) {
+                        case 'n': //irrelevant
+                            break;
+
+                        case 'k': //storeKey done
+                            // Command syntax: "k:<key>"
+                            worked = false;
+                            if (((key == null || key.equals("")) && param.length() == 32) && secured)
+                                worked = handler.storeKey(param);
+                            key = handler.key;
+                            break;
+
+                        case 'p': //storePW done
+                            // Command syntax: "p:(<hash>);<nonce>"
+                            worked = handler.storePW(param);
+                            oriHash = handler.oriHash;
+                            break;
+
+                        case 'c': //changePW done
+                            // Command syntax: "c:(<oldHash>;<newHash>);<nonce>"
+                            worked = handler.changePW(param);
+                            break;
+
+                        case 's': // setOTP done
+                            // Command syntax "s:(<otp>;<hash>);<nonce>>"
+                            worked = handler.setOTP(param);
+                            otps = handler.otps;
+                            break;
+
+                        case 'e': // einmalöffnung done
+                            // Command syntax: "e:<otp>;<time>"
+                            worked = handler.einmalOeffnung(param);
+                            otps = handler.otps;
+                            break;
+
+                        case 'a': // a für "password action" aka halts maul justin und formulier gescheit was du sagen willst du keks
+                            System.out.println("PaSsWoRd AcTiOn"); // this case is irrelevant
+                            System.out.println("Junge sag doch einfach, dass das als öffnen gemeint war"); // just ignore this
+                            Printer.printToFile(dateF.format(new Date()) + ": Das hätte definitiv nicht passieren sollen? #weirdflexbutok", "log.txt", true);
+                            break;
+
+                        case 'o': // Open  done
+                            // Command syntax: "o:(<hash>;<time>);<nonce>"
+                            worked = handler.open(param);
+                            break;
+
+                        case 'r': //reset
+                            // Command syntax: "r:(<hash>);<nonce>"
+                            worked = handler.reset(param);
+                            if (!worked) System.out.println("Das hat wohl nicht geklappt! :(");
+                            key = handler.key;
+                            oriHash = handler.oriHash;
+                            break;
+
+                        case 'H': // "how are you", get's called from alivekeeper, never from user lmao
+                            toClient.println("I'm fine, thanks");
+                            break;
+
+                        default:
+                            //also irrelevant
+                            System.out.println("hat wohl nich gegeht ¯\\_(ツ)_/¯ ");
+                            Printer.printToFile(dateF.format(new Date()) + ": Wie zur Hölle bist du hier gelandet??? #nochweirdererflex", "log.txt", true);
+                            Printer.printToFile(dateF.format(new Date()) + ": The whole message was: " + fromclient, "log.txt", true);
+                            toClient.println("What do you want from this poor Server? \uD83E\uDD7A");
+                            break;
+                    }
+                }
+                catch (Exception exc){
+                    exc.printStackTrace();
+                    worked = false;
                 }
 
+                toClient.println(worked + "EOS");
                 connected.close();
             }
 
